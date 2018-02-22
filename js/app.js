3fa26b540c7f9ef5868d59454afe65f331ad2fdc
Nohemi Barrera <nohemi.baol@gmail.com>
2018-02-22 00:58:20 -0600

    charts

---
+++ js/app.js
@@ -0,0 +1,20 @@
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Month', 'Sales'],
+          ['Oct',  1000],
+          ['Nov',  1170],
+          ['Dec',  660],
+          ['Jan',  1030]
+        ]);
+
+        var options = {
+          hAxis: {title: '',  titleTextStyle: {color: '#333'}},
+          vAxis: {minValue: 0}
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
